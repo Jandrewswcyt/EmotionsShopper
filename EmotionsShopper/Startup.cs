cb7c61f39df14e068aa66dde8ec6b87d0067d050
jonathan <jandrewswcyt@gmail.com>
2017-06-14 01:23:54 +0100

    Setting up repository

--- EmotionsShopper/Startup.cs
+++ EmotionsShopper/Startup.cs
@@ -1,20 +1,33 @@
-﻿using System.IO;
-using EmotionsShopper.DataTypes.Interfaces;
-using EmotionsShopper.Fakes;
+﻿using EmotionsShopper.DataTypes.Interfaces;
+using EmotionsShopper.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http; 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using EmotionsShopper.Repository;
+using EmotionsShopper.Fakes;
 
 namespace EmotionsShopper
 {
     public class Startup
     {
+
+        IConfigurationRoot Configuration; 
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder().SetBasePath(env.ContentRootPath).AddJsonFile("appsettings.json").Build(); 
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository,FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["Data:EmotionsShopperProducts:ConnectionString"])); 
+            services.AddTransient<IProductRepository,EFProductRepository>();
             services.AddMvc();
         }
 
@@ -28,6 +41,8 @@ namespace EmotionsShopper
             {
                 routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}");
             });
+
+            FakeSeedData.Populate(app); 
         }
     }
 }
