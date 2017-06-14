53316fc9b3a91b0926b2f2eda9b34d3210a6a94c
jonathan <jandrewswcyt@gmail.com>
2017-06-14 01:24:31 +0100

    added appsetting.json

---
+++ EmotionsShopper/Repository/EFProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using EmotionsShopper.DataTypes.Interfaces;
+using EmotionsShopper.Models;
+
+namespace EmotionsShopper.Repository
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext context;
+
+        public IEnumerable<Product> Products => context.Products; 
+    }
+}
