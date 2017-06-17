0acba36854d9d0f1a345816566c7b22e091f0724
jonathan <jandrewswcyt@gmail.com>
2017-06-17 17:43:51 +0100

    Added new route for pagination

--- EmotionsShopper/Startup.cs
+++ EmotionsShopper/Startup.cs
@@ -40,6 +40,7 @@ namespace EmotionsShopper
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
+                routes.MapRoute(name: "pagination", template: "Products/Page{page}", defaults: new { Controller = "Product", action = "List" }); 
                 routes.MapRoute(name: "default", template: "{controller=Product}/{action=List}/{id?}");
             });
 
