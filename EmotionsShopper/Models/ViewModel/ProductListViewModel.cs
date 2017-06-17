eee069798c58498201795bd5275dae477477839b
jonathan <jandrewswcyt@gmail.com>
2017-06-17 11:13:59 +0100

    Added Page Link helper and tests

---
+++ EmotionsShopper/Models/ViewModel/ProductListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmotionsShopper.Models.ViewModel
+{
+    public class ProductListViewModel
+    {
+
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfor { get; set;  }
+    }
+}
