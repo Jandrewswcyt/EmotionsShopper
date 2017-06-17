0e88645043d184f6de118baf800bc333c641d5dc
jonathan <jandrewswcyt@gmail.com>
2017-06-17 09:58:00 +0100

    Added basic support for pagination

--- EmotionsShopper/Controllers/ProductController.cs
+++ EmotionsShopper/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using EmotionsShopper.DataTypes.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq; 
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -7,14 +8,19 @@ namespace EmotionsShopper.Controllers
 {
     public class ProductController : Controller
     {
-        private IProductRepository repository; 
+        private IProductRepository repository;
+        public int ProductsPerPage = 4; 
 
         public ProductController(IProductRepository repo)
         {
             repository = repo; 
         }
 
-        public ViewResult List() => View(repository.Products); 
+        public ViewResult List( int page = 1) => View(repository.Products
+            .OrderBy
+            (p => p.ProductID) // order by id
+            .Skip((page - 1) * ProductsPerPage) // skip all products before
+            .Take(ProductsPerPage)); //take the number of products needed. 
 
         // GET: /<controller>/
         public IActionResult Index()
