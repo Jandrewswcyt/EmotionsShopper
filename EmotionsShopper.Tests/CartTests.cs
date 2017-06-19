e6dda1e85b14a2b61c910155a91b46142c683353
Jonathan Andrews <jonathanandrews@jonathans-mbp.home>
2017-06-19 21:02:18 +0100

    Added test for cart

--- EmotionsShopper.Tests/CartTests.cs
+++ EmotionsShopper.Tests/CartTests.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Linq;
+using EmotionsShopper.Models;
+using Xunit; 
 namespace EmotionsShopper.Tests
 {
     public class CartTests
     {
-        public CartTests()
+        [Fact]
+        public void Can_Add_Cart_Lines()
         {
-        }
+            //Arrange 
+            Product p1 = new Product { ProductID = 1, Name = "P1" }; 
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            Cart target = new Cart();
+
+            //Act 
+            target.AddItem(p1,1); 
+            target.AddItem(p2,2);
+
+            Cart.CartLine[] results = target.Lines.ToArray();
+
+            //Asset 
+            Assert.Equal(2,results.Length); 
+            Assert.Equal(p1,results[0].Prooduct);
+			Assert.Equal(p2, results[1].Prooduct);
+
+		}
     }
 }
--- EmotionsShopper/Models/Cart.cs
+++ EmotionsShopper/Models/Cart.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace EmotionsShopper.Models
 {
     public class Cart
     {
-        public Cart()
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
         {
+            CartLine line = lineCollection.FirstOrDefault(p => p.Prooduct.ProductID == product.ProductID);
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Prooduct = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
         }
+
+        public virtual void RemoveItem(Product product)
+        {
+            lineCollection.RemoveAll(l => l.Prooduct.Price == product.ProductID);
+        }
+
+        public virtual decimal GetTotalValue() => lineCollection.Sum(e => e.Prooduct.Price * e.Quantity);
+
+        public virtual void Clear() => lineCollection.Clear();
+        public virtual IEnumerable<CartLine> Lines => lineCollection;
+        public class CartLine
+        {
+            public int CartLineID { get; set; }
+            public Product Prooduct { get; set; }
+            public int Quantity { get; set; }
+        }
+
+
     }
 }
